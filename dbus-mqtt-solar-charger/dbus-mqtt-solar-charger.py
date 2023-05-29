55fb16895035d6055bb5c532ee3532d4b0493e68
Manuel <mr-manuel@outlook.it>
2023-05-29 20:53:07 +0200

    fix VRM error

--- dbus-mqtt-solar-charger/dbus-mqtt-solar-charger.py
+++ dbus-mqtt-solar-charger/dbus-mqtt-solar-charger.py
@@ -423,7 +423,7 @@ class DbusMqttSolarChargerService:
         self._dbusservice.add_path("/ProductId", 0xFFFF)
         self._dbusservice.add_path("/ProductName", productname)
         self._dbusservice.add_path("/CustomName", customname)
-        self._dbusservice.add_path("/FirmwareVersion", 1)
+        self._dbusservice.add_path("/FirmwareVersion", 399)
         self._dbusservice.add_path("/HardwareVersion", "1.0.0 (20230528)")
         self._dbusservice.add_path("/Connected", 1)
 
